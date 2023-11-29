80c261fdd5951dc0fc58fb77d5c26c0f5fbf7191
txdgidraph <gmwangi103@gmail.com>
2023-11-29 14:43:44 +0300

    Completed Menu and Navbar design

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,59 +1,22 @@
 import { Link } from "react-router-dom";
 import "./menu.scss";
+import { menu } from "../../data";
+
 const Menu = () => {
   return (
     <div className="menu">
-      <div className="item">
-        <span className="title">MAIN</span>
-        <Link to="/" className="listItem">
-          <img src="/home.svg" alt="" />
-          <span className="listItemTitle">Home</span>
-        </Link>
-        <Link to="/" className="listItem">
-          <img src="/profile.svg" alt="" />
-          <span className="listItemTitle">Profile</span>
-        </Link>
-      </div>
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
 
-      <div className="item">
-        <span className="title">LISTS</span>
-        <Link to="/" className="listItem">
-          <img src="/user.svg" alt="" />
-          <span className="listItemTitle">Users</span>
-        </Link>
-        <Link to="/" className="listItem">
-          <img src="/product.svg" alt="" />
-          <span className="listItemTitle">Products</span>
-        </Link>
-        <Link to="/" className="listItem">
-          <img src="/order.svg" alt="" />
-          <span className="listItemTitle">Orders</span>
-        </Link>
-        <Link to="/" className="listItem">
-          <img src="/post.svg" alt="" />
-          <span className="listItemTitle">Posts</span>
-        </Link>
-      </div>
-
-      <div className="item">
-        <span className="title">GENERAL</span>
-        <Link to="/" className="listItem">
-          <img src="/element.svg" alt="" />
-          <span className="listItemTitle">Elements</span>
-        </Link>
-        <Link to="/" className="listItem">
-          <img src="/note.svg" alt="" />
-          <span className="listItemTitle">Notes</span>
-        </Link>
-        <Link to="/" className="listItem">
-          <img src="/form.svg" alt="" />
-          <span className="listItemTitle">Forms</span>
-        </Link>
-        <Link to="/" className="listItem">
-          <img src="/calendar.svg" alt="" />
-          <span className="listItemTitle">Calendar</span>
-        </Link>
-      </div>
+          {item.listItems.map((listItem) => (
+            <Link to={listItem.ur1} className="listItem" key={listItem.id}>
+              <img src={listItem.icon} alt="" />
+              <span className="listItemTitle">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/data.ts
@@ -0,0 +1,122 @@
+export const menu = [
+  {
+    id: 1,
+    title: "Main",
+    listItems: [
+      {
+        id: 1,
+        title: "Homepage",
+        ur1: "/",
+        icon: "home.svg",
+      },
+      {
+        id: 2,
+        title: "Profile",
+        ur1: "/users/1",
+        icon: "user.svg",
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    title: "Lists",
+    listItems: [
+      {
+        id: 1,
+        title: "Users",
+        ur1: "/users",
+        icon: "user.svg",
+      },
+      {
+        id: 2,
+        title: "Products",
+        ur1: "/products",
+        icon: "product.svg",
+      },
+      {
+        id: 3,
+        title: "Orders",
+        ur1: "/",
+        icon: "order.svg",
+      },
+      {
+        id: 4,
+        title: "Posts",
+        ur1: "/",
+        icon: "post.svg",
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    title: "General",
+    listItems: [
+      {
+        id: 1,
+        title: "Elements",
+        ur1: "/",
+        icon: "element.svg",
+      },
+      {
+        id: 2,
+        title: "Notes",
+        ur1: "/",
+        icon: "note.svg",
+      },
+      {
+        id: 3,
+        title: "Forms",
+        ur1: "/",
+        icon: "form.svg",
+      },
+      {
+        id: 4,
+        title: "Calendar",
+        ur1: "/",
+        icon: "calendar.svg",
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    title: "Maintenance",
+    listItems: [
+      {
+        id: 1,
+        title: "Settings",
+        ur1: "/",
+        icon: "setting.svg",
+      },
+      {
+        id: 2,
+        title: "Backups",
+        ur1: "/",
+        icon: "backup.svg",
+      },
+    ],
+  },
+
+  {
+    id: 5,
+    title: "Analytics",
+    listItems: [
+      {
+        id: 1,
+        title: "Charts",
+        ur1: "/",
+        icon: "chart.svg",
+      },
+      {
+        id: 2,
+        title: "Logs",
+        ur1: "/",
+        icon: "log.svg",
+      },
+    ],
+  },
+];
+
+export default menu;
